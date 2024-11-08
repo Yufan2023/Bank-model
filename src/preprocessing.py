9e7851956d03925d32cd2ca3e3c8576898a253d8
Yufan2023 <148704737+Yufan2023@users.noreply.github.com>
2024-11-08 11:15:28 -0500

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,15 +1,22 @@
-import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
-def preprocess_data(data, sequence_length=60):
+def add_technical_indicators(data):
+    data['TD_20_MA'] = data['TD_Close'].rolling(window=20).mean()
+    data['TD_200_MA'] = data['TD_Close'].rolling(window=200).mean()
+    data.dropna(inplace=True)
+    return data
+
+def preprocess_data(data):
     scaler = MinMaxScaler(feature_range=(0, 1))
     data_scaled = scaler.fit_transform(data)
-    X = data_scaled[:, 1:]  # All columns except the target
-    y = data_scaled[:, 0]   # The target column
-    X_lstm, y_lstm = [], []
+    return data_scaled, scaler
 
+def reshape_for_lstm(data_scaled, sequence_length=60):
+    X = data_scaled[:, 1:]
+    y = data_scaled[:, 0]
+    X_lstm, y_lstm = [], []
     for i in range(sequence_length, len(X)):
         X_lstm.append(X[i-sequence_length:i])
         y_lstm.append(y[i])
-
-    return np.array(X_lstm), np.array(y_lstm), scaler
+    return np.array(X_lstm), np.array(y_lstm)
