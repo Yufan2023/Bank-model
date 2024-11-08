a7b4d7c7fcf9078b0bb363ccc158d10d4c533481
Yufan2023 <148704737+Yufan2023@users.noreply.github.com>
2024-11-08 11:13:51 -0500

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,15 @@
 import yfinance as yf
 import pandas as pd
 
-def download_data(tickers, start_date, end_date):
+def download_stock_data(tickers, start_date, end_date):
     data = pd.DataFrame()
     for column_name, ticker in tickers.items():
         stock_data = yf.download(ticker, start=start_date, end=end_date)
         data[column_name] = stock_data['Close']
+    return data
+
+def download_economic_data(data, economic_tickers, start_date, end_date):
+    for column_name, ticker in economic_tickers.items():
+        econ_data = yf.download(ticker, start=start_date, end=end_date)
+        data[column_name] = econ_data['Close']
     return data
